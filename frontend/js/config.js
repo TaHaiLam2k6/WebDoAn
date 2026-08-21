f3f96e65ed6442bfb9b1d67b0f57af56abb95d90
TaHaiLam2k6 <nguyentunglam1726@gmail.com>
2026-08-22 00:17:40 +0700

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,3 @@
 // Thay URL này bằng URL Web Service PHP của Render.
-const API_BASE = "https://YOUR-RENDER-SERVICE.onrender.com";
+// URL Web Service PHP trên Render
+const API_BASE = "https://webdoan-rkwq.onrender.com";
